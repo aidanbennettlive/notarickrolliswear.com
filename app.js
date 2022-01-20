55385066bcf1dc590171b5fba4abd1b323315a27
Aidan Bennett <aidanbennett1103@gmail.com>
2022-01-20 23:19:53 +0000

    Format Code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-  window.onload = () => {
-    setTimeout(() => {
-      window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    },3000)
-  }
+window.onload = () => {
+	setTimeout(() => {
+		window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+	}, 3000);
+};
